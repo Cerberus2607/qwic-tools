8a0af6912438283b653d5343311935d613129064
Cerberus2607 <r.abuisbeih@googlemail.com>
2026-08-02 17:12:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* QWIC Händler-Tools – Service Worker v17 */
-var CACHE = 'qwic-tools-v17';
+var CACHE = 'qwic-tools-v18';
 var ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'];
